0c78102aafb0c73a83266cb471ea74e95a87642f
SuLee <leeseunguk3@gmail.com>
2023-05-30 16:58:25 +0900

    0530_1

---
+++ join.js
@@ -0,0 +1,7 @@
+const submitBtn = document.getElementById('submitBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+
+cancelBtn.addEventListener('click', function(){
+    console.log('취소');
+    location.href = "file:///C:/Users/BD/project/vanilla/index.html"
+});
---
+++ login.js
@@ -0,0 +1,10 @@
+const loginBtn = document.getElementById("loginBtn");
+const joinBtn = document.getElementById("joinBtn");
+
+joinBtn.addEventListener('click', function(){
+    location.href='file:///C:/Users/BD/project/vanilla/join.html'
+});
+
+loginBtn.addEventListener('click', function(){
+    
+});
