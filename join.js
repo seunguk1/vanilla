7577741c13c7f4d044add70f89d7f653324f2fba
SuLee <leeseunguk3@gmail.com>
2023-05-30 18:06:31 +0900

    0530_2

--- join.js
+++ join.js
@@ -3,5 +3,50 @@ const cancelBtn = document.getElementById('cancelBtn');
 
 cancelBtn.addEventListener('click', function(){
     console.log('취소');
-    location.href = "file:///C:/Users/BD/project/vanilla/index.html"
+    location.href = "file:///C:/Users/BD/project/vanilla/login.html"
 });
+
+function submit(){
+    validation();
+
+    console.log("save");
+}
+
+function validation(){
+    const userId = document.getElementById('userId').value.trim();
+    const userPwd = document.getElementById('userPwd').value.trim();
+    const userPwd_chk = document.getElementById('userPwd_chk').value.trim();
+    const userName = document.getElementById('userName').value.trim();
+    const userAge = document.getElementById('userAge').value;
+
+    if(userId === ""){
+        alert("아이디를 입력해주세요.");
+        document.getElementById('userId').focus();
+        return false;
+    }else if(userPwd === ""){
+        alert("비밀번호를 입력해주세요.");
+        document.getElementById('userPwd').focus();
+        return false;
+    }else if(userPwd_chk === ""){
+        alert("비밀번호를 다시 한번 입력해주세요.");
+        document.getElementById('userPwd_chk').focus();
+        return false;
+    }else if(userPwd !== "" && userPwd_chk !== ""){
+        if(userPwd !== userPwd_chk){
+            document.getElementById('caution_1').style.display = 'block';
+            return false;
+        }else {
+            document.getElementById('caution_1').style.display = 'none';
+            return true;
+        }
+    }else if(userName === ""){
+        alert("이름을 입력해주세요.");
+        document.getElementById('userName').focus();
+        return false;
+    }else if(userAge === ""){
+        alert("나이를 선택해주세요.");
+        document.getElementById('userAge').focus();
+        return false;
+    }
+
+}
--- login.js
+++ login.js
@@ -1,10 +1,27 @@
-const loginBtn = document.getElementById("loginBtn");
 const joinBtn = document.getElementById("joinBtn");
 
 joinBtn.addEventListener('click', function(){
     location.href='file:///C:/Users/BD/project/vanilla/join.html'
 });
 
-loginBtn.addEventListener('click', function(){
+function login() {
+    const userId = document.getElementById('userId').value.trim();   
+    const userPwd = document.getElementById('userPwd').value.trim();   
     
-});
+    console.log('ID :: '+userId.length);
+    console.log('PW :: '+userPwd.length);
+
+    // 입력값 검증
+    if(userId === ""){
+        alert("아이디를 입력해주세요.");
+        document.getElementById('userId').focus();
+    }else if(userPwd === "") {
+        alert("비밀번호를 입력해주세요.");
+        document.getElementById('userPwd').focus();
+    }else {
+        // storage에 저장된 정보와 비교
+         
+        location.href='file:///C:/Users/BD/project/vanilla/myPage.html'
+    }
+
+}
